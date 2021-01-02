4e00c4af1cfb252b3559891a8f06f3c19b0a2189
seggas <035535@gmail.com>
2021-01-02 15:21:38 +0500

    Initial commit

---
+++ src/main/java/com/github/sparamonov/simplegameframework/SimpleGameFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.github.sparamonov.simplegameframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGameFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleGameFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/sparamonov/simplegameframework/SimpleGameFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sparamonov.simplegameframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGameFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
